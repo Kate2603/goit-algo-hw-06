fb9d2212d6100af6f2f0972a97e78a23ca346f9f
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2024-08-01 15:34:25 +0300

    fixed file task_3

--- task_3.py
+++ task_3.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import heapq
 
 # Створимо граф
 G = nx.Graph()
@@ -27,17 +28,51 @@ labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 plt.show()
 
-# Реалізація алгоритму Дейкстри для знаходження найкоротшого шляху між усіма вершинами
-def dijkstra_all_pairs(graph):
-    paths = dict(nx.all_pairs_dijkstra_path(graph))
-    lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
-    return paths, lengths
+# Реалізація алгоритму Дейкстри
+def dijkstra(graph, start):
+    # Ініціалізація
+    distances = {node: float('inf') for node in graph.nodes}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    shortest_path_tree = {}
 
-# Отримання найкоротших шляхів та їх довжин
-paths, lengths = dijkstra_all_pairs(G)
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, data in graph[current_node].items():
+            weight = data['weight']
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+                shortest_path_tree[neighbor] = current_node
+
+    return distances, shortest_path_tree
+
+# Знаходження найкоротших шляхів від кожної вершини до всіх інших
+all_pairs_shortest_paths = {}
+all_pairs_distances = {}
+
+for node in G.nodes:
+    distances, paths = dijkstra(G, node)
+    all_pairs_shortest_paths[node] = paths
+    all_pairs_distances[node] = distances
 
 # Виведення результатів
 print("Найкоротші шляхи між усіма вершинами:")
-for source in paths:
-    for target in paths[source]:
-        print(f"Шлях від {source} до {target}: {paths[source][target]}, довжина шляху: {lengths[source][target]}")
+for source in all_pairs_shortest_paths:
+    for target in G.nodes:
+        if source != target:
+            path = []
+            current_node = target
+            while current_node != source:
+                path.append(current_node)
+                current_node = all_pairs_shortest_paths[source][current_node]
+            path.append(source)
+            path.reverse()
+            print(f"Шлях від {source} до {target}: {' -> '.join(path)}, довжина шляху: {all_pairs_distances[source][target]}")
+
